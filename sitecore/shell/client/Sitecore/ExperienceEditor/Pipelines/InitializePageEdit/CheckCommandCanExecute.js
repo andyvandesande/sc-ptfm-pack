36acb60d3683fbd959603db46d789dde20253cd6
Andy van de Sande <andyvandesande@gmail.com>
2016-08-08 11:11:30 +0200

    First install

---
+++ sitecore/shell/client/Sitecore/ExperienceEditor/Pipelines/InitializePageEdit/CheckCommandCanExecute.js
@@ -0,0 +1,15 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+  return {
+    priority: 1,
+    execute: function (context) {
+      ExperienceEditor.CommandsUtil.runCommandsCollectionCanExecute(context.commands, function (stripId) {
+        var cookieValue = ExperienceEditor.Common.getCookieValue("sitecore_webedit_activestrip");
+        if (cookieValue === "") {
+          return false;
+        }
+
+        return stripId && stripId + "_ribbon_tab" !== cookieValue;
+      }, false);
+    }
+  };
+});
